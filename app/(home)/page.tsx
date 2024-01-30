36b940223bc5308f4a56bc3a04c48ead9bcfebf7
João Vitor Guedes <138164577+guedescss@users.noreply.github.com>
2024-01-30 20:15:30 -0300

    feat: add initial search components

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,7 +1,6 @@
 import { format } from "date-fns";
 import Header from "../_components/header";
 import { ptBR } from "date-fns/locale";
-import Search from "./_components/search";
 
 export default function Home() {
   return (
@@ -17,9 +16,6 @@ export default function Home() {
         })}
         </p>
      </div>
-        <div className="px-5 mt-6">
-        <Search />
-       </div>
    </div>
   );
 }
