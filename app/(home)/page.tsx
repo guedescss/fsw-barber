07467c1927c0c0a993b0aa8169211ed6aa2a4dc2
João Vitor Guedes <138164577+guedescss@users.noreply.github.com>
2024-01-31 14:37:32 -0300

    feat: initial barbershop item

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -3,8 +3,13 @@ import Header from "../_components/header";
 import { ptBR } from "date-fns/locale";
 import Search from "./_components/search"; 
 import BookingItem from "../_components/ui/booking-item";
+import { db } from "../_lib/prisma";
+import BarbershopItem from "./_components/barbershop-item";
 
-export default function Home() {
+
+export default async function Home() {
+  //chamar prisma e pegar barbearia
+  const barbershop = await db.barbershop.findMany({})
   return (
    <div>
     <Header/>
@@ -28,9 +33,16 @@ export default function Home() {
         <BookingItem />
       </div>
 
-      
+      <div className="mt-6">
+      <h2 className="px-5 text-xs mb-3 uppercase text-gray-400 font-bold">Recomendados</h2>
 
+      <div className="flex px-5 gap-4 overflow-x-auto [&::-webkit-scrollbar]:hidden">
+        {barbershop.map((barbershop) => (
+          <BarbershopItem key={barbershop.id} barbershop={barbershop} />
+        ))}
+      </div>
 
+      </div>
    </div>
   );
 }
