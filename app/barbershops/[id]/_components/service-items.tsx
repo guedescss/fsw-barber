267f12f65f9775ef32d24071d7343e2fe40ed9ce
João Vitor Guedes <138164577+guedescss@users.noreply.github.com>
2024-02-05 14:01:15 -0300

    feat: ensure confirmed bookings are shown on home page

--- app/barbershops/[id]/_components/service-items.tsx
+++ app/barbershops/[id]/_components/service-items.tsx
@@ -3,13 +3,25 @@
 import { Button } from "@/app/_components/ui/button";
 import { Card, CardContent } from "@/app/_components/ui/card";
 import { Service } from "@prisma/client";
+import { signIn } from "next-auth/react";
 import Image from "next/image";
 
 interface ServiceItemProps {
   service: Service;
+  isAuthenticated?: boolean;
 }
 
-const ServiceItem = ({ service }: ServiceItemProps) => {
+const ServiceItem = ({ service, isAuthenticated }: ServiceItemProps) => {
+
+
+  const handleBookingClick = () => {
+    if (!isAuthenticated) {
+      return signIn('google');
+
+    }
+
+    // TODO: abrir modal de agendamento
+  }
   return (
     <Card>
         <CardContent className="p-3">
@@ -29,7 +41,7 @@ const ServiceItem = ({ service }: ServiceItemProps) => {
                 currency: "BRL",
               }).format(Number(service.price))}
               </p>
-              <Button variant='secondary'>Reservar</Button>
+              <Button variant='secondary' onClick={handleBookingClick}>Reservar</Button>
 
 
               </div>
--- app/barbershops/[id]/page.tsx
+++ app/barbershops/[id]/page.tsx
@@ -1,9 +1,8 @@
-import { Button } from "@/app/_components/ui/button";
 import { db } from "@/app/_lib/prisma";
-import { ChevronLeftIcon, MapPinIcon, MenuIcon, StarIcon } from "lucide-react";
-import Image from "next/image";
 import BarbershopInfo from "./_components/barbershop-info";
 import ServiceItem from "./_components/service-items";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 
 interface BarbershopsDetailsPageProps {
   params: {
@@ -14,6 +13,8 @@ interface BarbershopsDetailsPageProps {
 const BarbershopsDetailsPage = async ({
   params,
 }: BarbershopsDetailsPageProps) => {
+
+const session =  await getServerSession(authOptions)
   if (!params.id) {
     // TODO: redirecionar pra home page
     return null;
@@ -40,8 +41,9 @@ const BarbershopsDetailsPage = async ({
 
     <div className="px-5 flex flex-col gap-4 py-6">
     {barbershop.services.map((service) => (
-        <ServiceItem key={service.id} service={service}/>
+        <ServiceItem key={service.id} service={service} isAuthenticated={!!session?.user}/>
     ))}
+    
     </div>
   </div>
   );
