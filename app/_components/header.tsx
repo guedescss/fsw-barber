642ae79b213e34a32ef2a4b18357a1d4957f8440
João Vitor Guedes <138164577+guedescss@users.noreply.github.com>
2024-02-08 13:47:25 -0300

    feat: ensure confirmed bookings are shown on home page

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -4,28 +4,26 @@ import Image from "next/image";
 import { Card, CardContent } from "./ui/card";
 import { Button } from "./ui/button";
 import { MenuIcon } from "lucide-react";
-import {
-  Sheet,
-  SheetContent,
-  SheetTrigger,
-} from "./ui/sheet";
-import { signIn, signOut, useSession } from "next-auth/react";
+import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
 import SideMenu from "./side-menu";
+import Link from "next/link";
 
 const Header = () => {
   return (
     <Card>
       <CardContent className="p-5 justify-between items-center flex flex-row">
-        <Image src="/logo.png" alt="FSW Barber" height={22} width={120} />
+        <Link href="/">
+          <Image src="/logo.png" alt="FSW Barber" height={18} width={120} />
+        </Link>
         <Sheet>
           <SheetTrigger asChild>
             <Button variant="outline" size="icon">
-              <MenuIcon />
+              <MenuIcon size={16} />
             </Button>
           </SheetTrigger>
 
           <SheetContent className="p-0">
-                <SideMenu />
+            <SideMenu />
           </SheetContent>
         </Sheet>
       </CardContent>
