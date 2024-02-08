d5729e0c37c1e2ed0f4905dfd222afb7436d6729
João Vitor Guedes <138164577+guedescss@users.noreply.github.com>
2024-02-08 13:20:52 -0300

    feat: add bookings page

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,57 +1,56 @@
 import { format } from "date-fns";
 import Header from "../_components/header";
 import { ptBR } from "date-fns/locale";
-import Search from "./_components/search"; 
+import Search from "./_components/search";
 import BookingItem from "../_components/booking-item";
 import { db } from "../_lib/prisma";
 import BarbershopItem from "./_components/barbershop-item";
 
-
 export default async function Home() {
-  //chamar prisma e pegar barbearia
-  const barbershop = await db.barbershop.findMany({})
+  // chamar prisma e pegar barbearias
+  const barbershops = await db.barbershop.findMany({});
+
   return (
-   <div>
-    <Header/>
+    <div>
+      <Header />
 
-    <div className="px-5 pt-5">
-
-      <h2 className="text-xl font-bold">Olá, João</h2>
-      <p className="capitalize text-sm">
-        {format(new Date(), "EEEE',' dd 'de' MMMM",{
-          locale: ptBR,
-        })}
+      <div className="px-5 pt-5">
+        <h2 className="text-xl font-bold">Olá, Miguel!</h2>
+        <p className="capitalize text-sm">
+          {format(new Date(), "EEEE',' dd 'de' MMMM", {
+            locale: ptBR,
+          })}
         </p>
-     </div>
-     <div className="px-5 mt-6">
+      </div>
+
+      <div className="px-5 mt-6">
         <Search />
       </div>
 
-
-      <div className="px-5 mt-6">
+      {/* <div className="px-5 mt-6">
         <h2 className="text-xs mb-3 uppercase text-gray-400 font-bold">Agendamentos</h2>
         <BookingItem />
-      </div>
+      </div> */}
 
       <div className="mt-6">
-      <h2 className="px-5 text-xs mb-3 uppercase text-gray-400 font-bold">Recomendados</h2>
+        <h2 className="px-5 text-xs mb-3 uppercase text-gray-400 font-bold">Recomendados</h2>
 
-      <div className="flex px-5 gap-4 overflow-x-auto [&::-webkit-scrollbar]:hidden">
-        {barbershop.map((barbershop) => (
-          <BarbershopItem key={barbershop.id} barbershop={barbershop} />
-        ))}
-      </div>
+        <div className="flex px-5 gap-4 overflow-x-auto [&::-webkit-scrollbar]:hidden">
+          {barbershops.map((barbershop) => (
+            <BarbershopItem key={barbershop.id} barbershop={barbershop} />
+          ))}
+        </div>
       </div>
 
       <div className="mt-6 mb-[4.5rem]">
-      <h2 className="px-5 text-xs mb-3 uppercase text-gray-400 font-bold">Populares</h2>
+        <h2 className="px-5 text-xs mb-3 uppercase text-gray-400 font-bold">Populares</h2>
 
-      <div className="flex px-5 gap-4 overflow-x-auto [&::-webkit-scrollbar]:hidden">
-        {barbershop.map((barbershop) => (
-          <BarbershopItem key={barbershop.id} barbershop={barbershop} />
-        ))}
+        <div className="flex px-5 gap-4 overflow-x-auto [&::-webkit-scrollbar]:hidden">
+          {barbershops.map((barbershop) => (
+            <BarbershopItem key={barbershop.id} barbershop={barbershop} />
+          ))}
+        </div>
       </div>
-      </div>
-   </div>
+    </div>
   );
 }
