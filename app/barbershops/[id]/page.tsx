b81e1f1d5cf52c5723330ec6a0631e0ffab2d116
João Vitor Guedes <138164577+guedescss@users.noreply.github.com>
2024-02-09 20:50:55 -0300

    fix: type error

--- app/barbershops/[id]/page.tsx
+++ app/barbershops/[id]/page.tsx
@@ -1,8 +1,8 @@
 import { db } from "@/app/_lib/prisma";
 import BarbershopInfo from "./_components/barbershop-info";
-import ServiceItem from "./_components/service-item";
 import { getServerSession } from "next-auth";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { authOptions } from "@/app/_lib/auth";
+import ServiceItem from "./_components/service-items";
 
 interface BarbershopDetailsPageProps {
   params: {
